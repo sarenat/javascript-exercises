ad3de2e92318343b01e13ec15888991ecc8bbfff
sarenat <sarenattran@gmail.com>
2025-10-25 13:57:19 -0400

    Complete first 6 exercises of 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeat) {
+    if (repeat<0) return "ERROR";
+    
+    let repeatedString = "";
+    for (i=1; i<=repeat; i++) repeatedString = repeatedString.concat(str);
+    return repeatedString;
 };
 
 // Do not edit below this line
