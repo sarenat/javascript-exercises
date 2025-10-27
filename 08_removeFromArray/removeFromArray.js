fb342cb32b978ad58da02cbc10f0cb40d9d6d2cf
sarenat <sarenattran@gmail.com>
2025-10-27 16:32:46 -0400

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = [...arguments];
+
+            for (i=arr.length; i>=0; i--) {
+                if (args.includes(arr[i])) {
+                    arr.splice(i, 1);
+                }
+            }
+            return arr;
 };
 
 // Do not edit below this line
