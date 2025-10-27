de2afe1ab12ae0ab06d90fdf8191f7ea46f3a286
sarenat <sarenattran@gmail.com>
2025-10-27 19:13:33 -0400

    Complete 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit-32)*(5/9);
+  if (!Number.isInteger(celsius)) return Number(celsius.toFixed(1));
+  else return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius*(9/5) + 32;
+  if (!Number.isInteger(celsius)) return Number(fahrenheit.toFixed(1));
+  else return fahrenheit;
 };
 
 // Do not edit below this line
