7dd92e6e3c79ed319b3b7b40975c94c918cf785e
sarenat <sarenattran@gmail.com>
2025-10-27 18:48:59 -0400

    Complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a<0 || b<0) return "ERROR";
+    if (typeof(a) != "number" || typeof(b) != "number") return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+    let small, large = 0;
+    if (a<b) {
+        small = a; large = b;
+    } else {
+        large = a; small = b;
+    }
+
+    let range = [];
+    for (i=small; i<=large; i++) {
+        range = range.concat(i);
+    }
+
+    return range.reduce((sum, current) => sum + current, 0);
 };
 
 // Do not edit below this line
