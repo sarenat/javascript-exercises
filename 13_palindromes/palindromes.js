038bd234a7aa74c849f1538a0e0ee29a19c32c64
sarenat <sarenattran@gmail.com>
2025-12-02 16:19:38 -0500

    Complete 12_calculator and 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').replaceAll(' ', '');
+    let reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;  
 };
 
 // Do not edit below this line
