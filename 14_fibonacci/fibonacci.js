ae0de75a0f8bc30be9f1b3b844cb3d507f3e39bc
sarenat <sarenattran@gmail.com>
2025-12-02 21:42:33 -0500

    Try to push changes to 14_fibonacci" q

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x = Number(x);
+    let arr = [1, 1];
 
+    if (x < 0) return "OOPS";
+    else if (x == 0) return 0;
+    else {
+        for (let i = 0; i < 30; i++) {
+            let next = arr[arr.length-1] + arr[arr.length-2];
+            arr.push(next);
+        }
+        return arr[x-1];
+    }
 };
 
 // Do not edit below this line
