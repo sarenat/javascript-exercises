8b08676b21f4e0825c7e2e16fec78732d5ec050e
sarenat <sarenattran@gmail.com>
2025-12-02 21:41:51 -0500

    Try to push changes to 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product*current, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factorial = 1;
+  if (x == 0) return factorial;
+  else {
+    for (let i = x; i > 0; i--) {
+      factorial = factorial * i;
+    };
+    return factorial;
+  }
 };
 
 // Do not edit below this line
